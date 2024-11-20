ariankordi/ffl

commit bc2bed76d17695b914dd5ceb614ff808170fa150

break out RIO interop into FFLRIOInterop.h

include/nn/ffl/FFLRIOInterop.h
@@ -1,0 +1,55 @@
+#ifndef FFL_RIO_INTEROP_H_
+#define FFL_RIO_INTEROP_H_
+
+#include <nn/ffl/FFLVec.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Substitutes for RIO structs that are used in public headers
+
+// float 4x4 matrix
+typedef struct FFLRIOBaseMtx44f
+{
+	union
+	{
+		// rio::BaseMtx44<float>
+		f32         m[4][4];
+		f32         a[4*4];
+		FFLVec4     v[4];
+	};
+}
+FFLRIOBaseMtx44f;
+
+typedef u32 FFLRIOCompareFunc; // TODO: not tested
+#if RIO_IS_CAFE
+typedef GX2PrimitiveMode FFLRIOPrimitiveMode; // TODO: not tested
+#elif RIO_IS_WIN
+typedef unsigned int FFLRIOPrimitiveMode; // should map directly..
+#endif
+// only needed for below structure
+#if RIO_IS_CAFE
+typedef const GX2Texture* FFLRIONativeTexture2DHandle;
+typedef GX2Texture FFLRIONativeTexture2D;
+#elif RIO_IS_WIN
+typedef unsigned int FFLRIONativeTexture2DHandle;
+// below is SOLELY used as padding - rio::NativeTexture2D does NOT have pointers (only a u64?) so this constant size should be fine
+typedef u8 FFLRIONativeTexture2D[128]; // sizeof(rio::NativeTexture2D)
+#endif
+
+typedef struct FFLRIOTexture2D
+{
+	FFLRIONativeTexture2D mTextureInner;
+	// Only mHandle is needed to bind the texture on OpenGL
+	FFLRIONativeTexture2DHandle mHandle; // OpenGL texture handle
+	bool mSelfAllocated;
+} FFLRIOTexture2D;
+
+#define FFL_GET_RIO_NATIVE_TEXTURE_HANDLE(texture2D) (texture2D)->mHandle
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FFL_RIO_INTEROP_H_

include/nn/ffl/types.h
@@ -114,47 +114,7 @@
     static_assert(sizeof(f64) == 8);
     // end of rio_Types.h typedefs
 
-    // float 4x4 matrix
-    #include <nn/ffl/FFLVec.h>
-    typedef struct FFLRIOBaseMtx44f
-    {
-        union
-        {
-            // rio::BaseMtx44<float>
-            f32         m[4][4];
-            f32         a[4*4];
-            FFLVec4     v[4];
-        };
-    }
-    FFLRIOBaseMtx44f;
-
-    typedef u32 FFLRIOCompareFunc; // TODO: not tested
-#if RIO_IS_CAFE
-    typedef GX2PrimitiveMode FFLRIOPrimitiveMode; // TODO: not tested
-#elif RIO_IS_WIN
-    typedef unsigned int FFLRIOPrimitiveMode; // should map directly..
-#endif
-    // only needed for below structure
-#if RIO_IS_CAFE
-    typedef const GX2Texture* FFLRIONativeTexture2DHandle;
-    typedef GX2Texture FFLRIONativeTexture2D;
-#elif RIO_IS_WIN
-    typedef unsigned int FFLRIONativeTexture2DHandle;
-    // below is SOLELY used as padding - rio::NativeTexture2D does NOT have pointers (only a u64?) so this constant size should be fine
-    typedef u8 FFLRIONativeTexture2D[128]; // sizeof(rio::NativeTexture2D)
-#endif
-
-    // Substitutes for RIO structs that are used in public headers
-    typedef struct FFLRIOTexture2D
-    {
-        FFLRIONativeTexture2D mTextureInner;
-        // Only mHandle is needed to bind the texture on OpenGL
-        FFLRIONativeTexture2DHandle mHandle; // OpenGL texture handle
-        bool mSelfAllocated;
-    } FFLRIOTexture2D;
-
-    #define FFL_GET_RIO_NATIVE_TEXTURE_HANDLE(texture2D) (texture2D)->mHandle
-
+    #include <nn/ffl/FFLRIOInterop.h> // includes substitutes for rio types
 #endif
 
 

commit 730144f9e92116a38385622a7f568244b346b92c

fix FFL exports for C (oops)
, that were broken since bc2bed76d17695b914dd5ceb614ff808170fa150 i
think arrghhh

include/nn/ffl/FFLDrawParam.h
@@ -7,6 +7,16 @@
     #include <math/rio_Matrix.h>
 #endif
 
+#if FFL_USE_RIO
+    #include <gpu/rio_Drawer.h>
+    #ifndef FFL_RIO_PRIMITIVE_MODE_
+        #define FFL_RIO_PRIMITIVE_MODE_
+        typedef rio::Drawer::PrimitiveMode FFLRIOPrimitiveMode;
+    #endif
+#else
+    #include <nn/ffl/FFLRIOInterop.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif

include/nn/ffl/FFLModulateParam.h
@@ -2,6 +2,7 @@
 #define FFL_MODULATE_PARAM_H_
 
 #include <nn/ffl/types.h>
+#include <nn/ffl/FFLTexture.h>
 
 #ifdef __cplusplus
 extern "C" {

include/nn/ffl/FFLRIOInterop.h
@@ -1,6 +1,7 @@
 #ifndef FFL_RIO_INTEROP_H_
 #define FFL_RIO_INTEROP_H_
 
+#include <nn/ffl/types.h>
 #include <nn/ffl/FFLVec.h>
 
 #ifdef __cplusplus
@@ -44,7 +45,8 @@ typedef struct FFLRIOTexture2D
 	// Only mHandle is needed to bind the texture on OpenGL
 	FFLRIONativeTexture2DHandle mHandle; // OpenGL texture handle
 	bool mSelfAllocated;
-} FFLRIOTexture2D;
+}
+FFLRIOTexture2D;
 
 #define FFL_GET_RIO_NATIVE_TEXTURE_HANDLE(texture2D) (texture2D)->mHandle
 

include/nn/ffl/FFLShaderCallback.h
@@ -3,6 +3,16 @@
 
 #include <nn/ffl/types.h>
 
+#if FFL_USE_RIO
+    #include <gfx/rio_Graphics.h>
+    typedef rio::Graphics::CompareFunc FFLRIOCompareFunc;
+
+    #include <math/rio_Matrix.h>
+    typedef rio::BaseMtx44f FFLRIOBaseMtx44f;
+#else
+    #include <nn/ffl/FFLRIOInterop.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif

include/nn/ffl/FFLTexture.h
@@ -1,0 +1,15 @@
+#ifndef FFL_TEXTURE_H_
+#define FFL_TEXTURE_H_
+
+
+#if FFL_USE_RIO
+	#include <gpu/rio_Texture.h>
+	typedef rio::Texture2D FFLTexture;
+	#define FFL_GET_RIO_NATIVE_TEXTURE_HANDLE(texture2D) (texture2D)->getNativeTextureHandle()
+#else
+	#include <nn/ffl/FFLRIOInterop.h>
+	typedef FFLRIOTexture2D FFLTexture;
+#endif
+
+
+#endif // FFL_TEXTURE_H_

include/nn/ffl/types.h
@@ -45,16 +45,12 @@
 
 // NN_STATIC_ASSERT32 = NN_STATIC_ASSERT but only applies for 32 bit
 #ifdef __cplusplus
-    #include <type_traits>
-
     #if defined(NDEBUG) || (!defined(__WUT__) && INTPTR_MAX == INT64_MAX)
         #define NN_STATIC_ASSERT32(condition) static_assert(true, "")
     #else
         #define NN_STATIC_ASSERT32 NN_STATIC_ASSERT
     #endif
 #else // __cplusplus
-    #include <assert.h>
-
     #if defined(NDEBUG) || INTPTR_MAX == INT64_MAX
         #define NN_STATIC_ASSERT32(condition) _Static_assert(true, "")
     #else
@@ -64,19 +60,12 @@
 
 
 #ifdef __cplusplus
+    #define FFL_USE_RIO 1
     #include <misc/rio_Types.h>
-    // Define typedefs that are exported in public headers
     #include <gpu/rio_Texture.h>
     typedef rio::Texture2D FFLRIOTexture2D;
-    #include <math/rio_Matrix.h>
-    typedef rio::BaseMtx44f FFLRIOBaseMtx44f;
-    #include <gfx/rio_Graphics.h>
-    typedef rio::Graphics::CompareFunc FFLRIOCompareFunc;
-    #include <gpu/rio_Drawer.h>
-    typedef rio::Drawer::PrimitiveMode FFLRIOPrimitiveMode;
-
-    #define FFL_GET_RIO_NATIVE_TEXTURE_HANDLE(texture2D) (texture2D)->getNativeTextureHandle()
 #else
+    #define FFL_USE_RIO 0
     // Typedefs that RIO would have otherwise imported
 
     #if defined(__WUT__)
@@ -89,7 +78,6 @@
 
     #include <stdint.h>
     #include <stddef.h>
-    #include <assert.h>
     #include <stdbool.h>
     typedef  int8_t s8;
     typedef uint8_t u8;
@@ -105,7 +93,7 @@
 
     typedef float  f32;
     typedef double f64;
-
+    /*
     static_assert(sizeof(s8)  == sizeof(u8)  && sizeof(u8)  == sizeof(char) && sizeof(char) == 1);
     static_assert(sizeof(s16) == sizeof(u16) && sizeof(u16) == 2);
     static_assert(sizeof(s32) == sizeof(u32) && sizeof(u32) == 4);
@@ -113,8 +101,7 @@
     static_assert(sizeof(f32) == 4);
     static_assert(sizeof(f64) == 8);
     // end of rio_Types.h typedefs
-
-    #include <nn/ffl/FFLRIOInterop.h> // includes substitutes for rio types
+    */
 #endif
 
 
@@ -183,7 +170,7 @@ enum
 };
 
 // __int128 will only be defined on gnu, 64-bit
-#if !RIO_IS_CAFE && __SIZEOF_INT128__
+#if !RIO_IS_CAFE && __SIZEOF_INT128__ && !defined(_MSC_VER)
 typedef __int128 FFLExpressionFlag;
 #define FFL_EXPRESSION_LIMIT FFL_EXPRESSION_MAX
 #else